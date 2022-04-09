27a419b3908688c7ac3010abbf707a3471e80b9a
Marek Pisch <60669435+marekDeveloper@users.noreply.github.com>
2022-04-09 17:20:24 -0600

    Adds custom-table-import-csv.php
    Adding main plugin file with plugin header information

---
+++ custom-table-import-csv.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name:       Custom Table Import CSV
+ * Plugin URI:        https://github.com/marekDeveloper/WP-CustomTableImportCSV
+ * Description:       Example WP plugin to create custom DB table and import CSV data into this table
+ * Version:           0.1
+ * Author:            Marek Pisch
+ * Author URI:        http://www.altaso.com
+ */
+
